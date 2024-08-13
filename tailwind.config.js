b0a59f76e9466cf77f7f54c0bbe60f00845cff94
Jonghyun Lee <126178027+studioayo@users.noreply.github.com>
2024-08-13 18:41:33 +0900

    first commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,10 +7,15 @@ module.exports = {
   ],
   theme: {
     extend: {
+      colors: {
+        "pastell-purple": "#EFF1FF",
+      },
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+        "gradient-cta":
+          "linear-gradient(180deg, rgba(239, 241, 255, 0.00) 0%, #EFF1FF 100%)",
       },
     },
   },
